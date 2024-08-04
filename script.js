71b8f27255359f4d83457c241028028e5518bb95
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-08-04 18:07:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -40,3 +40,55 @@ images.forEach((img) =>{
 	
 	
 })
+
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+        cy.visit('https://course.acciojob.com/open-web-editor?question=bd6d3416-91a7-4ece-878c-2d643c773d4f'); // Replace with your actual URL
+    });
+
+    it('All div exists', () => {
+        cy.get('#drag1').should('exist');
+        cy.get('#drag2').should('exist');
+        cy.get('#drag3').should('exist');
+        cy.get('#drag4').should('exist');
+        cy.get('#drag5').should('exist');
+        cy.get('#drag6').should('exist');
+    });
+
+    it('Drag and drop 3rd image on 6th', () => {
+        cy.get('#drag3').trigger('dragstart');
+        cy.get('#drag6').trigger('drop');
+        cy.get('#drag6').should('contain', 'Image 3');
+    });
+
+    it('Drag and drop 1st image on 5th', () => {
+        cy.get('#drag1').trigger('dragstart');
+        cy.get('#drag5').trigger('drop');
+        cy.get('#drag5').should('contain', 'Image 1');
+    });
+
+    it('Drag and drop 4th image on 2nd', () => {
+        cy.get('#drag4').trigger('dragstart');
+        cy.get('#drag2').trigger('drop');
+        cy.get('#drag2').should('contain', 'Image 4');
+    });
+
+    it('Drag and drop 2nd image on 3rd', () => {
+        cy.get('#drag2').trigger('dragstart');
+        cy.get('#drag3').trigger('drop');
+        cy.get('#drag3').should('contain', 'Image 2');
+    });
+
+    it('Drag and drop 5th image on 3rd', () => {
+        cy.get('#drag5').trigger('dragstart');
+        cy.get('#drag3').trigger('drop');
+        cy.get('#drag3').should('contain', 'Image 5');
+    });
+
+    it('Drag and drop 6th image on 1st', () => {
+        cy.get('#drag6').trigger('dragstart');
+        cy.get('#drag1').trigger('drop');
+        cy.get('#drag1').should('contain', 'Image 6');
+    });
+});
